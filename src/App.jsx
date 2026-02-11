2760decfecb9a3751781a88df230e1055e2b5975
Aquiles Alves <aquilespereira789@gmail.com>
2026-02-11 14:14:08 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
 function App() {
   return (
     <div>
-      <h1>Gerenciador de Tarefas</h1>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
+      <AddTask/>
+      <Tasks />
+
     </div>
   );
 }
